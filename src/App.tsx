83ed9ab5fac72584ba75fbe76cd8246891823d50
rbcunha <barrosdacunha@gmail.com>
2023-01-01 10:11:52 -0300

    feat: adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
+import { ThemeProvider } from '@mui/material'
+import { LigthTheme } from './shared/themes'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LigthTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
