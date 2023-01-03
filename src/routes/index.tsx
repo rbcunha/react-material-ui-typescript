2f235b6ad79479c5294b6e07ab588824d98dd70d
rbcunha <barrosdacunha@gmail.com>
2023-01-03 16:51:30 -0300

    feat: adiconada responsividade no menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,26 @@
 import { Button } from '@mui/material'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { useAppThemeContext } from '../shared/contexts'
+import { useDrawerContext } from '../shared/contexts'
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext()
+  const { toggleDrawerOpen } = useDrawerContext()
 
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary" onClick={toggleTheme}>
-            ToggleTheme
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
+            Toggle Drawer
           </Button>
         }
       />
-
-      <Route path="*" element={<Navigate to="/pagina-inicial" />} />npm install --legacy-peer-deps
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      npm install --legacy-peer-deps
     </Routes>
   )
 }
