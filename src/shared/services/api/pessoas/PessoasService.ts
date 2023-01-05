e55da90835db2274e6e675659bfcfa4fd473a5b8
rbcunha <barrosdacunha@gmail.com>
2023-01-05 16:24:51 -0300

    feat: estruturando método get

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -1,15 +1,48 @@
 import { Environment } from "../../../environment";
 import { Api } from "../axios-config";
 
+interface IListagemPessoa {
+  id: number;
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
 
-const getAll = async (page = 1, filter = ''): Promise<any> => {
+interface IDetalhePessoa {
+  id: number;
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
+
+type TPessoaComTotalCount = {
+  data: IListagemPessoa[];
+  totalCount: number;
+
+}
+
+
+const getAll = async (page = 1, filter = ''): Promise<TPessoaComTotalCount | Error> => {
   try {
 
     const urlRelativa = `/pessoas?_page=${page}&_limnit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like${filter}`;
 
-    const { data } = await Api.get(urlRelativa);
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(headers['x-total-count'] || 
+        Environment.LIMITE_DE_LINHAS),
+      };
+    }
+
+    return new Error('Erro ao listar os registros!');
     
   } catch (error) {
+    console.error(error);
+
+    return new Error((error as {message:string}).message || 'Erro ao listar os registros!');
     
   }
 };
