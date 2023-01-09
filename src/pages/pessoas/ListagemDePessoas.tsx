7954aa85d723fec4a937fe2d6e65e1a5a86e2a7d
rbcunha <barrosdacunha@gmail.com>
2023-01-09 16:47:30 -0300

    feat: implementação do hook useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,24 +4,29 @@ import { useSearchParams } from "react-router-dom";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { UseDebounce } from "../../shared/hooks/UseDebounce";
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const {debounce} = UseDebounce(3000, false)
 
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-          return;
-        } else {
-         console.log(result);
-        }
-      });
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+            return;
+          } else {
+           console.log(result);
+          }
+        });
+    });
   }, [busca]);
 
   return (
